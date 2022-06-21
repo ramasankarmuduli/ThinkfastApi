a2700ca12300da20a6561ba7804ef8fd2503ed5d
Ramasankar Muduli <ramasankarmuduli5@gmail.com>
2022-06-21 16:04:36 +0530

    update and delete api

--- src/controllers/common.controller.js
+++ src/controllers/common.controller.js
@@ -3,9 +3,97 @@ const mongoose = require('mongoose');
 
 async function saveDoc(req, res) {
     const CommonModel = mongoose.model(req.body.collection, models.Common);
-    const commonDoc = new CommonModel(JSON.parse(JSON.stringify(req.body.data)));
-    commonDoc.save(function (error, response) {
-        if(error) {
+    if (req.body.meta && req.body.meta.multiInsert) {
+        var insertData = JSON.parse(JSON.stringify(req.body.data));
+        CommonModel.insertMany(insertData)
+            .then(function (response) {
+                let result = {
+                    status: 'success',
+                    message: '',
+                    data: response
+                };
+                res.json(result);
+            }).catch(function (error) {
+                let result = {
+                    status: 'failed',
+                    message: '',
+                }
+                res.json(result);
+            });
+    } else {
+        const commonDoc = new CommonModel(JSON.parse(JSON.stringify(req.body.data)));
+        commonDoc.save(function (error, response) {
+            if (error) {
+                let result = {
+                    status: 'failed',
+                    message: '',
+                }
+                res.json(result);
+            } else {
+                let result = {
+                    status: 'success',
+                    message: '',
+                    data: response
+                };
+                res.json(result);
+            }
+        });
+    }
+
+}
+
+async function fetchDoc(req, res) {
+    let collection = req.query.collection;
+    //let limit = req.query.limit;
+    const CommonModel = mongoose.model(collection, models.Common);
+    CommonModel.find({}, function (error, response) {
+        if (error) {
+            console.log(error);
+            let result = {
+                status: 'failed',
+                message: '',
+            }
+        } else {
+            let result = {
+                status: 'success',
+                message: '',
+                data: response
+            };
+            res.json(result);
+        }
+    });
+}
+
+async function fetchDocById(req, res) {
+    let collection = req.query.collection;
+    let id = req.params.id;
+    const CommonModel = mongoose.model(collection, models.Common);
+    CommonModel.findById(id, function (error, response) {
+        if (error) {
+            console.log(error);
+            let result = {
+                status: 'failed',
+                message: '',
+            }
+        } else {
+            let result = {
+                status: 'success',
+                message: '',
+                data: response
+            };
+            res.json(result);
+        }
+    });
+}
+
+async function updateDoc(req, res) {
+    const CommonModel = mongoose.model(req.body.collection, models.Common);
+    var docId = req.body.id;
+    var docData = JSON.parse(JSON.stringify(req.body.data));
+    var options = { 'upsert': false, returnOriginal: false };
+    CommonModel.findByIdAndUpdate(docId, docData, options, function (error, response) {
+        console.log('response', response)
+        if (error) {
             let result = {
                 status: 'failed',
                 message: '',
@@ -22,44 +110,21 @@ async function saveDoc(req, res) {
     });
 }
 
-async function fetchDoc(req, res) {
-    let collection = req.query.collection;
-    //let limit = req.query.limit;
-    const CommonModel = mongoose.model(collection, models.Common);
-    CommonModel.find({}, function (error, response) {
-        if (error){
-            console.log(error);
-            let result = {
-                status: 'failed',
-                message: '',
-            }
-        } else{
-            let result = {
-                status: 'success',
-                message: '',
-                data: response
-            };
-            res.json(result);
-        }
-    });
-}
-
-async function fetchDocById(req, res) {
+async function deleteDocById(req, res) {
     let collection = req.query.collection;
     let id = req.params.id;
     const CommonModel = mongoose.model(collection, models.Common);
-    CommonModel.findById(id, function (error, response) {
-        if (error){
+    CommonModel.findByIdAndRemove(id, function (error, response) {
+        if (error) {
             console.log(error);
             let result = {
                 status: 'failed',
                 message: '',
             }
-        } else{
+        } else {
             let result = {
                 status: 'success',
-                message: '',
-                data: response
+                message: 'Deleted Successfully'
             };
             res.json(result);
         }
@@ -69,5 +134,7 @@ async function fetchDocById(req, res) {
 module.exports = {
     saveDoc,
     fetchDoc,
-    fetchDocById
+    fetchDocById,
+    updateDoc,
+    deleteDocById
 }
--- src/routes/curd.js
+++ src/routes/curd.js
@@ -3,8 +3,10 @@ const controllers = require('../controllers');
 const router = express.Router();
 const validateToken = require("../middleware/auth");
 
-router.post('/saveDoc', validateToken, controllers.common.saveDoc);
-router.get('/fetchDoc', validateToken, controllers.common.fetchDoc);
-router.get('/fetchDoc/:id', validateToken, controllers.common.fetchDocById);
+router.post('/doc', validateToken, controllers.common.saveDoc);
+router.get('/doc', validateToken, controllers.common.fetchDoc);
+router.get('/doc/:id', validateToken, controllers.common.fetchDocById);
+router.put('/doc', validateToken, controllers.common.updateDoc);
+router.delete('/doc/:id', validateToken, controllers.common.deleteDocById);
 
 module.exports = router;
